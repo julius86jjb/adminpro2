3a367a66066071e54713d0fe2ebd27e98416f1c3
Julio Jimenez <julius.1986@gmail.com>
2023-08-29 23:12:54 +0100

    URL_SERVCIOS produccion

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,2 @@
-export const URL_SERVICIOS = 'http://localhost:3000';
+// export const URL_SERVICIOS = 'http://localhost:3000';
+export const URL_SERVICIOS = 'https://backend-adminpro-express.up.railway.app';
